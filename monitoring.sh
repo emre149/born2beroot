6a58425ae7f7ec11a0288891574dd7def21a53bf
Dedemoglu Emre <emre@Emre.local>
2024-02-20 17:33:01 +0100

    finished babe

--- monitoring.sh
+++ monitoring.sh
@@ -5,8 +5,8 @@
 #                                                     +:+ +:+         +:+      #
 #    By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/02/19 20:00:28 by ededemog          #+#    #+#              #
-#    Updated: 2024/02/20 12:27:44 by ededemog         ###   ########.fr        #
+#    Created: 2024/02/20 17:32:38 by ededemog          #+#    #+#              #
+#    Updated: 2024/02/20 17:32:49 by ededemog         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ printf "\n\n"
 kernel_version=$(uname -r)
 archi=$(uname -m)
 
-cpu=$(lscpu | grep Socket | awk '{print $2}')
+cpu=$(lscpu | awk '/Socket/ {print $2}')
 vcpu=$(nproc)
 
 memtotal=$(free -m | grep Mem | awk '{print $2}')
@@ -25,15 +25,15 @@ mempercent=$(free -m | grep Mem | awk '{print $3/$2 * 100}')
 
 disktotal=$(df -h --block-size=G --total | tail -n 1 | awk '{printf $2}' | cut -d G -f1)
 diskused=$(df -h --block-size=G --total | tail -n 1 | awk '{printf $3}' | cut -d G -f1)
-diskpercent=$(df -h --block-size=G --total | tail -n 1 | awk '{printf $5}' | cut -d % -f1)
+diskpercent=$(df -h --block-size=G --total | tail -n 1 | awk '{print $5}' | cut -d % -f1)
 
-cpuload1=$(mpstat | tail -n 1 | awk '{gsub(/,/, "." $4); print $4}')
-cpuload2=$(mpstat | tail -n 1 | awk '{gsub(/,/, "." $6); print $6}')
+cpuload1=$(mpstat | tail -n 1 | awk '{gsub(/,/, ".", $4); print $4}')
+cpuload2=$(mpstat | tail -n 1 | awk '{gsub(/,/, ".", $6); print $6}')
 cpuload=$(echo "$cpuload1 + $cpuload2" | bc)
 
 lastreboot=$(who -b | awk '{print $3 " " $4}')
 
-lvm=$(cat /etc/fstab | grep /dev/mapper | wc -1)
+lvm=$(cat /etc/fstab | grep /dev/mapper | wc -l)
 
 tcp=$(echo "$(ss -t state established | wc -l) - 1" | bc)
 
@@ -42,22 +42,30 @@ usercount=$(who | wc -l)
 mac=$(ip address show | grep link/ether | awk '{print $2}')
 ip=$(ip address | grep enp | grep inet | awk '{print $2}' | cut -d / -f1)
 
+totalsudo=$(echo "obase=10; ibase=36; $(cat /var/log/sudo/seq)" | bc)
+
 printf "\t #Architecture : $archi $kernel_version\n"
-printf "\t #pCPU : $cpu\n"
-printf "\t #vCPU : $vcpu\n"
-printf "\t #Memory Usage : $memused/$memtotal%s ($mempercent%%)\n" "MB"
-printf "\t #Disk Usage : $diskused/$disktotal%s ($diskpercent%%)\n" "Gb"
-printf "\t #CPU Load : $cpuload%%\n"
-printf "\t #Last Reboot : $lastreboot\n"
+printf "\t #pCPU : %d\n" $cpu
+printf "\t #vCPU : %d\n" $vcpu
+printf "\t #Memory Usage : %d/%dMB (%.2f%%)\n" $memused $memtotal $mempercent
+printf "\t #Disk Usage : %d/%dGb (%.2f%%)\n" $diskused $disktotal $diskpercent
+if [[ $(echo "cpuload < 1" | bc -l) -eq 1 ]]
+then
+    printf "\t #CPU Load : 0%s%%\n" $cpuload
+else
+    printf "\t #CPU Load : %s%%\n" $cpuload
+fi
+printf "\t #Last Reboot : %s %s\n" $lastreboot
 printf "\t #LVM Use : "
 if [ $lvm -gt 0 ]
 then
-	printf "yes\n"
+    printf "yes\n"
 else
-	printf "no\n"
+    printf "no\n"
 fi
-printf "\t #TCP : $tcp ESTABLISHED\n"
-printf "\t #User Logs : $usercount\n"
-printf "\t #Network : IP $ip%s ($mac)\n"
+printf "\t #TCP : %d ESTABLISHED\n" $tcp
+printf "\t #User Logs : %d\n" $usercount
+printf "\t #Network : IP %s (%s)\n" $ip $mac
+printf "\t #Sudo : %d cmd\n" $totalsudo
 
 printf "\n\n"
